ac4ebcaa8ed664ed70e2cfdee258f5859853986c
llo23481 <david.perzie@gmail.com>
2025-06-17 16:32:17 -0400

    fix: conexión a Render con sslmode=require

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://visor_dicom_db_user:5YAnZH0EFMQ0XYxFL8qHlzgh6VfM4pld@dpg-d18r0o15pdvs73cs2b50-a.oregon-postgres.render.com/visor_dicom_db"
+DATABASE_URL = "postgresql://visor_dicom_db_user:5YAnZHOEfMQQYXfL8qHnCdQTs69Qi1Gm@dpg-d18r0o15pdvs73cs2b50-a.oregon-postgres.render.com/visor_dicom_db?sslmode=require"
 
-engine = create_engine(DATABASE_URL + "?sslmode=require")  # <- Aquí agregamos SSL
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
